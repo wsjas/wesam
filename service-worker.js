25a4da2e9c42c72975c5b4ccdfe0ac44beb1cb17
wsjas <wsjas@yahoo.com>
2025-12-01 23:52:50 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+const CACHE = "wesam-cache-v1";
+const ASSETS = [
+  "./WESAM.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=> r || fetch(e.request))
+  );
+});
